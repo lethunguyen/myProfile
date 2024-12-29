15bbc29377dbdc854a74d652bc130022c13c2e31
lethunguyen <nguyen.le-thu.phd@ieee.org>
2024-12-29 23:31:56 +0700

    test

---
+++ lib/myprofile.dart
@@ -0,0 +1,18 @@
+class Myprofile{
+  String date ="24/12/2024";
+  String workOfPlace ="Dai Hoc Phenikaa";
+  String jobTitle ="Giang vien";
+
+
+
+void setMyProfile(String date, String workOfPlace, String jobTitle){
+  this.date = date;
+  this.workOfPlace = workOfPlace;
+  this.jobTitle = jobTitle;
+}
+String getMyProfile(){
+  return workOfPlace;
+}
+
+
+}
